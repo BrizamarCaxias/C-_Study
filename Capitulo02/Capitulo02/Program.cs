696f6ae4033c5305899509f30ebde557f6e322be
Francisco Brizamar Caxias Sousa <brizamarcaxias@gmail.com>
2022-09-06 00:16:37 -0300

    Correcao commit

---
+++ Capitulo02/Capitulo02/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Capitulo02
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            Instituicao instituicao = new Instituicao();
+            System.Console.WriteLine("Informe o nome da instituição:");
+            instituicao.Nome = System.Console.ReadLine();
+            System.Console.WriteLine("Informe o endereco da instituição:");
+            instituicao.Endereco = System.Console.ReadLine();
+
+            System.Console.WriteLine("#####################################################");
+            System.Console.WriteLine($"Obrigado por informar os dados para a instituição {instituicao.Nome}");
+            System.Console.WriteLine("Pressione qualquer tecla para sair.");
+            System.Console.ReadKey();
+        }
+    }
+
+}
